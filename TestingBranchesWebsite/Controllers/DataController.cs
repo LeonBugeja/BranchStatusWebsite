d3372a9cc33c9da45b67830142b612654c5a63f1
LeonBugeja <114917740+LeonBugeja@users.noreply.github.com>
2023-11-29 23:27:18 +0100

    Add files via upload

---
+++ TestingBranchesWebsite/Controllers/DataController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Data.SQLite;
+using System.Data;
+using System.Web.Services;
+using System.Security.Cryptography;
+using System.Text;
+using System.IO;
+
+public class DataController : Controller
+{
+  public JsonResult CheckLogin(string username, string password)
+  {
+    string connectionString = $"Data Source={Server.MapPath("~/App_Data/webdata.db")};Version=3;";
+    var key = "b14ca5898a4e4133bbce2ea2315a1916";
+
+    using (SQLiteConnection db = new SQLiteConnection(connectionString))
+    {
+      db.Open();
+
+      string query = "SELECT * FROM login WHERE username = @username";
+      using (SQLiteCommand command = new SQLiteCommand(query, db))
+      {
+        command.Parameters.AddWithValue("@username", username);
+
+        using (SQLiteDataReader reader = command.ExecuteReader())
+        {
+          if (reader.Read()) //if it reads, username is found
+          {
+            string storedPassword = reader["password"].ToString();
+            storedPassword = DecryptString(key, storedPassword);
+
+            if (storedPassword == password)
+            {
+              return Json(new { success = true });
+            }
+            else
+            {
+              return Json(new { success = false });
+            }
+          }
+          else
+          {
+            return Json(new { success = false });
+          }
+        }
+      }
+    }
+  }
+
+
+  public static string DecryptString(string key, string cipherText)
+  {
+    byte[] iv = new byte[16];
+    byte[] buffer = Convert.FromBase64String(cipherText);
+
+    using (Aes aes = Aes.Create())
+    {
+      aes.Key = Encoding.UTF8.GetBytes(key);
+      aes.IV = iv;
+      ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+      using (MemoryStream memoryStream = new MemoryStream(buffer))
+      {
+        using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+        {
+          using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+          {
+            return streamReader.ReadToEnd();
+          }
+        }
+      }
+    }
+  }
+
+  /*public string EncryptString()
+  {
+    var key = "b14ca5898a4e4133bbce2ea2315a1916"; byte[] iv = new byte[16];
+    var plainText = "DRAdminMalta";
+
+    byte[] array;
+
+    using (Aes aes = Aes.Create())
+    {
+      aes.Key = Encoding.UTF8.GetBytes(key);
+      aes.IV = iv;
+
+      ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+      using (MemoryStream memoryStream = new MemoryStream())
+      {
+        using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
+        {
+          using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+          {
+            streamWriter.Write(plainText);
+          }
+
+          array = memoryStream.ToArray();
+        }
+      }
+    }
+
+    return Convert.ToBase64String(array);
+  }*/
+
+}
---
+++ TestingBranchesWebsite/Scripts/MainJavascript.js
@@ -0,0 +1,82 @@
+﻿function login() {
+    let userInput = document.getElementById("username").value;
+    let passInput = document.getElementById("password").value;
+
+    $.ajax({
+        type: "POST",
+        url: "/Data/CheckLogin",
+        data: { username: userInput, password: passInput },
+        success: function (response) {
+            if (response.success) {
+                //go to home page
+                //isLogged = true;
+                sessionStorage.setItem("isLogged", "true");
+                window.location.href = "/Home/Branches";
+            } else {
+                document.getElementById("login-outcome").innerHTML = "Incorrect username or password";
+            }
+        },
+        error: function () {
+            document.getElementById("login-outcome").innerHTML = "An error occurred";
+        }
+    });
+}
+
+
+//if user is in branches (home page)
+if (document.URL.toLowerCase().includes("home/branches")) {
+    loadBranches();
+}
+
+function loadBranches() {
+    var isLogged = sessionStorage.getItem("isLogged");
+    //check if user bypassed login page. if not logged in, take user to login page
+    if (isLogged == "true") {
+        //load branches
+        const branches = ["Test_Example", "Branch789", "Master"]; //TODO add new function to get branches and store them here
+        branches.forEach(createBranch);
+    } else {
+        window.location.href = "/";
+    }
+}
+
+function createBranch(item) {
+    var branchesContainer = document.getElementById("branches");
+
+    var containerDiv = document.createElement("div");
+    containerDiv.classList.add("container", "branch");
+
+    var title = document.createElement("p");
+    title.textContent = item;
+
+    var hr = document.createElement("hr");
+    hr.classList.add("container", "branch-divider");
+
+    var branchStatusDiv = document.createElement("div");
+    branchStatusDiv.id = "branch-status";
+
+    var detailsButton = document.createElement("button");
+    detailsButton.classList.add("container", "branch-details");
+    detailsButton.textContent = "Details";
+
+    var runButton = document.createElement("button");
+    runButton.classList.add("container", "branch-run");
+    runButton.textContent = "Run";
+
+    containerDiv.appendChild(title);
+    containerDiv.appendChild(hr);
+    containerDiv.appendChild(branchStatusDiv);
+    containerDiv.appendChild(detailsButton);
+    containerDiv.appendChild(runButton);
+
+    document.body.appendChild(containerDiv);
+    branchesContainer.appendChild(containerDiv);
+
+    var status = document.getElementById("branch-status").style;
+
+    //if branch passed
+    if (true) {
+        //set status led to green
+        status.cssText = "background-color: #00ff82; box-shadow: rgba(0, 0, 0, 0.2) 0 -1px 7px 1px, inset #3bc868 0 -1px 9px, #89FF00 0 2px 12px;";
+    }
+}
